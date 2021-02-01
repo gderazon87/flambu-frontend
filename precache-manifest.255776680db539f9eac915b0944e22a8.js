bb186412ac370ad07d10b834f3aa171bddad3e5a
full developer <developer1215@outlook.com>
2021-02-01 17:52:21 +0700

    'deploy'

---
+++ precache-manifest.255776680db539f9eac915b0944e22a8.js
@@ -0,0 +1,198 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e84d96482b47802e0a57",
+    "url": "/flambu-frontend/css/app.8d4e3d70.css"
+  },
+  {
+    "revision": "8b6af8e5e8324edfd77af8b3b35d7f9c",
+    "url": "/flambu-frontend/fonts/Poppins-Regular.8b6af8e5.ttf"
+  },
+  {
+    "revision": "475a26749ff9de9207ac37c4b37fa381",
+    "url": "/flambu-frontend/fonts/fa-brands-400.475a2674.eot"
+  },
+  {
+    "revision": "4acb4cc4916ee2758bcefee86e4814fe",
+    "url": "/flambu-frontend/fonts/fa-brands-400.4acb4cc4.woff"
+  },
+  {
+    "revision": "99f3121dc63babcf368cb6aee3b535f6",
+    "url": "/flambu-frontend/fonts/fa-brands-400.99f3121d.woff2"
+  },
+  {
+    "revision": "b00fd7dbf7c0a93f8c7add8d51b337d8",
+    "url": "/flambu-frontend/fonts/fa-brands-400.b00fd7db.ttf"
+  },
+  {
+    "revision": "1e4c8262b85b385157e764b89920f7f5",
+    "url": "/flambu-frontend/fonts/fa-light-300.1e4c8262.woff"
+  },
+  {
+    "revision": "64805970c4c65f0d9162355b020e33ce",
+    "url": "/flambu-frontend/fonts/fa-light-300.64805970.eot"
+  },
+  {
+    "revision": "67557ec3e55de3bba6df3562dad47ae9",
+    "url": "/flambu-frontend/fonts/fa-light-300.67557ec3.woff2"
+  },
+  {
+    "revision": "69ecb2bd9d986fdc442f0657eb251f57",
+    "url": "/flambu-frontend/fonts/fa-light-300.69ecb2bd.ttf"
+  },
+  {
+    "revision": "b856172881a708c644eb030168551e73",
+    "url": "/flambu-frontend/fonts/fa-regular-400.b8561728.woff"
+  },
+  {
+    "revision": "dd9e15837df7cd19a34455f5c54ce379",
+    "url": "/flambu-frontend/fonts/fa-regular-400.dd9e1583.eot"
+  },
+  {
+    "revision": "ea1125e0a2bd57c4bdcf2c7e20386767",
+    "url": "/flambu-frontend/fonts/fa-regular-400.ea1125e0.woff2"
+  },
+  {
+    "revision": "f18935cdad9fd0543247abe466075237",
+    "url": "/flambu-frontend/fonts/fa-regular-400.f18935cd.ttf"
+  },
+  {
+    "revision": "00d3012700332144ce43a62a3b7ec4f1",
+    "url": "/flambu-frontend/fonts/fa-solid-900.00d30127.woff2"
+  },
+  {
+    "revision": "3f7e3ac8288953f489dd3706da61b1c8",
+    "url": "/flambu-frontend/fonts/fa-solid-900.3f7e3ac8.eot"
+  },
+  {
+    "revision": "9242dedf4090cbdc546f036ac9b002da",
+    "url": "/flambu-frontend/fonts/fa-solid-900.9242dedf.ttf"
+  },
+  {
+    "revision": "dddc7b4661a865059856beb6658595aa",
+    "url": "/flambu-frontend/fonts/fa-solid-900.dddc7b46.woff"
+  },
+  {
+    "revision": "782bda6572a9c1780fa67b6d8eb4a174",
+    "url": "/flambu-frontend/img/app-store.782bda65.png"
+  },
+  {
+    "revision": "cdfebc78169edc8695d0170394e99118",
+    "url": "/flambu-frontend/img/avatar.cdfebc78.png"
+  },
+  {
+    "revision": "f28f372b5846d45af67edeb518108141",
+    "url": "/flambu-frontend/img/blog-news.f28f372b.png"
+  },
+  {
+    "revision": "5407ddba05537c2700a713012ca2e9f8",
+    "url": "/flambu-frontend/img/blog1.5407ddba.png"
+  },
+  {
+    "revision": "7fa3c0c5001c071b789ed48f31f0ee96",
+    "url": "/flambu-frontend/img/category1.7fa3c0c5.png"
+  },
+  {
+    "revision": "93ab0a9fdce18ed6585bae5683fa30d9",
+    "url": "/flambu-frontend/img/contact-map.93ab0a9f.png"
+  },
+  {
+    "revision": "5cb6e721dcddaa505a4b2623b924e2a2",
+    "url": "/flambu-frontend/img/draw_contact_us.5cb6e721.png"
+  },
+  {
+    "revision": "a9aa54d6c1a1acdb863e5df51e4c3ce7",
+    "url": "/flambu-frontend/img/enjoy-without-buying.a9aa54d6.png"
+  },
+  {
+    "revision": "82110f5de8b68af485945c9eed920734",
+    "url": "/flambu-frontend/img/fa-brands-400.82110f5d.svg"
+  },
+  {
+    "revision": "b6e4c70226d33b7990bbbb39dde2ba40",
+    "url": "/flambu-frontend/img/fa-light-300.b6e4c702.svg"
+  },
+  {
+    "revision": "1c840fa5ab2437a43f2094fce69f8310",
+    "url": "/flambu-frontend/img/fa-regular-400.1c840fa5.svg"
+  },
+  {
+    "revision": "ad34988acc704c67667121ca56ba6266",
+    "url": "/flambu-frontend/img/fa-solid-900.ad34988a.svg"
+  },
+  {
+    "revision": "54235f3356e28d370b20aac33e2ad01a",
+    "url": "/flambu-frontend/img/flame-logo.54235f33.png"
+  },
+  {
+    "revision": "724b746b3012e478299379fe5deb9800",
+    "url": "/flambu-frontend/img/flame-phone.724b746b.png"
+  },
+  {
+    "revision": "3456651924a818f8845b3a5a266ca923",
+    "url": "/flambu-frontend/img/google-playstore.34566519.png"
+  },
+  {
+    "revision": "f003ef58ade419cc8b117c4758dd61ce",
+    "url": "/flambu-frontend/img/home-share-flame.f003ef58.png"
+  },
+  {
+    "revision": "0b900b39dada5a84ba2191ec4395ece5",
+    "url": "/flambu-frontend/img/item-map.0b900b39.png"
+  },
+  {
+    "revision": "05a2a7c2c766714275d3d63ba764992d",
+    "url": "/flambu-frontend/img/item1.05a2a7c2.png"
+  },
+  {
+    "revision": "8d348822426aad815b280c444a8319f1",
+    "url": "/flambu-frontend/img/passion-lead-us.8d348822.png"
+  },
+  {
+    "revision": "b735cb5161a366165029365a6a76484f",
+    "url": "/flambu-frontend/img/relatedItem1.b735cb51.png"
+  },
+  {
+    "revision": "5bcf3bb633e845bbaa7da3a6646da6df",
+    "url": "/flambu-frontend/img/story-bg.5bcf3bb6.png"
+  },
+  {
+    "revision": "0e1be63c084432aee26341bfda068511",
+    "url": "/flambu-frontend/img/sustainable.0e1be63c.png"
+  },
+  {
+    "revision": "7372f62b2dcac5698e1a726d6fe211c1",
+    "url": "/flambu-frontend/img/top-bar-bg.7372f62b.png"
+  },
+  {
+    "revision": "58f0887089b0c494419a2402f6cc26ec",
+    "url": "/flambu-frontend/img/transportation.58f08870.png"
+  },
+  {
+    "revision": "1246b4bf8f44312f38924a60c6cd389c",
+    "url": "/flambu-frontend/img/travel-light.1246b4bf.png"
+  },
+  {
+    "revision": "99a60b715d4bc0e601945f0b6d206bf8",
+    "url": "/flambu-frontend/img/try-flambu-mobile.99a60b71.png"
+  },
+  {
+    "revision": "423ba511ba6a00cb7bcf764d9e527f01",
+    "url": "/flambu-frontend/index.html"
+  },
+  {
+    "revision": "e84d96482b47802e0a57",
+    "url": "/flambu-frontend/js/app.e3902323.js"
+  },
+  {
+    "revision": "67b5c77e9eb9dc33b9fc",
+    "url": "/flambu-frontend/js/chunk-vendors.8840bef2.js"
+  },
+  {
+    "revision": "2fb3b0cff60d25ae9f7337afc9cbe6ac",
+    "url": "/flambu-frontend/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/flambu-frontend/robots.txt"
+  }
+]);
